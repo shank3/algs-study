e111915c538b3983284add04bbc295049ea14e4f
shank3 <1754406632@qq.com>
2019-05-25 17:02:38 +0800

    leetcode 20,有效的括号

---
+++ src/test/java/leetcode/p020/SolutionTest.java
@@ -0,0 +1,19 @@
+package leetcode.p020;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void isValid() {
+        Solution s = new Solution();
+        assertTrue(s.isValid("()"));
+        assertTrue(s.isValid("()[]{}"));
+        assertTrue(s.isValid("([]){}"));
+        assertTrue(s.isValid("([{}])"));
+        assertFalse(s.isValid("([)]"));
+        assertFalse(s.isValid("(])"));
+    }
+}
