5fa261ee2b60df09730e3dac2a61f1f1f136c11d
shank3 <1754406632@qq.com>
2019-05-26 17:59:25 +0800

    leetcode 39,组合之和

---
+++ src/main/java/leetcode/p035/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p035;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 35.组合总和
+ */
+public class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> listAll = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, listAll, 0, new ArrayList<Integer>());
+        return listAll;
+    }
+
+    private void backtrack(int[] candidates, int target, List<List<Integer>> listAll, int i, ArrayList<Integer> tmp) {
+        if (target == 0) {
+            listAll.add(tmp);
+            return;
+        }
+        for (int j = i; j < candidates.length && candidates[j] <= target; j++) {
+            ArrayList<Integer> list = new ArrayList<Integer>(tmp);
+            list.add(candidates[j]);
+            backtrack(candidates, target - candidates[j], listAll, j, list);
+        }
+    }
+
+}
