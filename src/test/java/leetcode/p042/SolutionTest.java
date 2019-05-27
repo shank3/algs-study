3e8927df4ce3bfc175930f8d85fb4d930ffedeef
shank3 <1754406632@qq.com>
2019-05-27 11:57:54 +0800

    leetcode 42,接雨水

---
+++ src/test/java/leetcode/p042/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.p042;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void trap() {
+        Solution s = new Solution();
+        assertEquals(6, s.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
