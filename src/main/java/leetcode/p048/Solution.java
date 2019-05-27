087bce652fce39815ea73a49432a76ac497da682
shank3 <1754406632@qq.com>
2019-05-27 17:51:43 +0800

    leetcode 48,旋转头像

--- src/main/java/leetcode/p048/Solution.java
+++ src/main/java/leetcode/p048/Solution.java
@@ -9,10 +9,10 @@ public class Solution {
         for (int i = 0; i < n/2; i++) {
             for (int j = i; j < n-i-1; j++) {
                 int t = matrix[i][j];
-                matrix[i][n-j-1] = matrix[i][j];
-                matrix[i][n-j-1] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = t;
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = t;
             }
         }
     }
--- src/test/java/leetcode/p048/SolutionTest.java
+++ src/test/java/leetcode/p048/SolutionTest.java
@@ -1,5 +1,31 @@
+package leetcode.p048;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    @Test
+    public void rotate() {
+        int x = 1;
+        int n = 3;
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = x++;
+            }
+        }
+        Solution s = new Solution();
+        s.rotate(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
