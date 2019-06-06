e7399d22afaa6abea0f2b4f64b41d8a3a3438492
shank3 <1754406632@qq.com>
2019-06-06 14:18:59 +0800

    leetcode 96,不同的二叉搜索树

---
+++ src/test/java/leetcode/p096/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.p096;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void numTrees() {
+        Solution s = new Solution();
+        assertEquals(5, s.numTrees(3));
+    }
+}
