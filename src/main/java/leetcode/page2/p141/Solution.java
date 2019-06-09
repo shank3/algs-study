42bf31e39acaf261716fe486cdd9f86c6ff43827
shank3 <1754406632@qq.com>
2019-06-09 19:12:59 +0800

    leetcode 141,环形链表

---
+++ src/main/java/leetcode/page2/p141/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.page2.p141;
+
+import leetcode.ListNode;
+
+/**
+ * 141.环形链表
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        boolean cycle = false;
+        if (head == null || head.next == null) {
+            return cycle;
+        }
+        ListNode slow = head,fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return cycle;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return !cycle;
+    }
+}
