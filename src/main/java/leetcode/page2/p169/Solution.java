2d3e6cba2e16464fc0d886cdcd2af5d8a20e25cd
shank3 <1754406632@qq.com>
2019-06-11 17:22:02 +0800

    leetcode 169,求众数

---
+++ src/main/java/leetcode/page2/p169/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.page2.p169;
+
+/**
+ * 169.求众数
+ */
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1, maj = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == maj) {
+                count++;
+            } else {
+                if (--count == 0) {
+                    maj = nums[i + 1];
+                }
+            }
+        }
+        return maj;
+    }
+}
