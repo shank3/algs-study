7524b3af520d4a2a6d09b6b1ebffc1d3cf685a5c
shank3 <1754406632@qq.com>
2019-06-14 15:33:59 +0800

    leetcode 207,课程表

---
+++ src/main/java/leetcode/page3/p207/Solution.java
@@ -0,0 +1,49 @@
+package leetcode.page3.p207;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 207.课程表
+ */
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 0) {
+            return false;
+        }
+        int plen = prerequisites.length;
+        if (plen == 0) {
+            return true;
+        }
+        int[] inDegree = new int[numCourses];
+        for (int[] p : prerequisites) {
+            inDegree[p[0]]++;
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        // 首先加入入度为 0 的结点
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.addLast(i);
+            }
+        }
+        // 拓扑排序的结果
+        List<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Integer num = queue.removeFirst();
+            res.add(num);
+            // 把邻边全部遍历一下
+            for (int[] p : prerequisites) {
+                if (p[1] == num) {
+                    inDegree[p[0]]--;
+                    if (inDegree[p[0]] == 0) {
+                        queue.addLast(p[0]);
+                    }
+                }
+            }
+        }
+        // System.out.println("拓扑排序结果：");
+        // System.out.println(res);
+        return res.size() == numCourses;
+    }
+}
