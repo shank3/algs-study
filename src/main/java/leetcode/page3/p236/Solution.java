d0385a78fa1a64f2b659c05553e399425882941a
shank3 <1754406632@qq.com>
2019-06-16 22:48:18 +0800

    ✅leetcode 236,二叉树的最近公共祖先

---
+++ src/main/java/leetcode/page3/p236/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.page3.p236;
+
+import leetcode.TreeNode;
+
+/**
+ * 236.二叉树的最近公共祖先
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+}
