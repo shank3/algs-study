5af3126701116a78494da770bb82b25da03ef2b2
shank3 <1754406632@qq.com>
2019-06-22 22:33:15 +0800

    ✅leetcode 309,最佳买卖股票时机含冷冻期

--- src/test/java/leetcode/page4/p301/SolutionTest.java
+++ src/test/java/leetcode/page4/p301/SolutionTest.java
@@ -1,5 +1,16 @@
+package leetcode.page4.p301;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    @Test
+    public void removeInvalidParentheses() {
+        Solution s = new Solution();
+        assertEquals(2, s.removeInvalidParentheses("()())()").size());
+        assertEquals(2, s.removeInvalidParentheses("(a)())()").size());
+        assertEquals(1, s.removeInvalidParentheses(")(").size());
+    }
 }
