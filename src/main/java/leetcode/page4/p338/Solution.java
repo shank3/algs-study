3a472cf634774fd88aa3bb0929f423b178f078be
shank3 <1754406632@qq.com>
2019-06-23 20:23:21 +0800

    ✅leetcode 338,比特位计数

---
+++ src/main/java/leetcode/page4/p338/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.page4.p338;
+
+/**
+ * 338.比特位计数
+ */
+public class Solution {
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+        res[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            if ((i & 1) == 1) {
+                res[i] = res[i - 1] + 1;
+            } else {
+                res[i] = res[i / 2];
+            }
+        }
+        return res;
+    }
+}
