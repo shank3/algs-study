e51d261099b2392fa275747471a344439cbb0528
shank3 <1754406632@qq.com>
2019-06-28 14:56:28 +0800

    ✅leetcode 461,汉明距离

---
+++ src/test/java/leetcode/page4/p461/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.page4.p461;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void hammingDistance() {
+        Solution s = new Solution();
+        assertEquals(2, s.hammingDistance(1, 4));
+    }
+}
