90bee2bd7d3c02b349c1ac1684b1144a14ee12ff
shank3 <1754406632@qq.com>
2019-10-16 14:35:43 +0800

    util

---
+++ src/main/java/util/ConfigUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConfigUtil {
+
+    public static Set<String> getWhiteIPs() {
+        Set<String> s = new HashSet<>();
+        //get white ip from config
+        return s;
+    }
+}
